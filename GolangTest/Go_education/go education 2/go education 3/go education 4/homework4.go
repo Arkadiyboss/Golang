341ae8d405ae10aed0583712d6fb6850aaa066a7
arkadiy <arkadiybosstrue@yandex.ru>
2026-04-04 14:16:24 +0300

    homework4

--- GolangTest/Go_education/go education 2/go education 3/go education 4/homework4.go
+++ GolangTest/Go_education/go education 2/go education 3/go education 4/homework4.go
@@ -2,20 +2,27 @@ package main
 
 import "fmt"
 
-// Person содержит имя и возраст человека.
 type Person struct {
-	Name string
-	Age int
+    Name string
+    Age  int
 }
-
-// TODO: реализуйте Stringer для Person
-
+type Family struct {
+    Name string
+    Age  int
+}
+type human interface {
+    nameAge()
+}
+func (p Person) String() string {
+    return fmt.Sprintf("%s (%d)", p.Name, p.Age)
+}
+func (p Person) nameAge()  {
+}
+func (f Family) nameAge()  {
+}
 func main() {
-	alice := Person{Name: "Alice", Age: 30}
-	fmt.Println(alice)
-	// Ожидаемый вывод: Alice (30)
-}
-
-type Stringer interface {
-    Stringer() string
+    alice := Person{Name: "Alice", Age: 30}
+    boris := Family{Name: "Boris", Age: 20}
+    fmt.Println(human(alice))
+    fmt.Print(human(boris))
 }
