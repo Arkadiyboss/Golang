2c3aa5043e8df4d1a305cfca9f5620eff643c5e9
arkadiy <arkadiybosstrue@yandex.ru>
2026-06-26 18:24:21 +0300

    commit EZ

--- GolangTest/Go_education/OOP/OOP2/homework2.go
+++ GolangTest/Go_education/OOP/OOP2/homework2.go
@@ -8,7 +8,6 @@ package main
 
 import (
     "fmt"
-    "math"
 )
 
 // Shape – интерфейс для геометрических фигур
--- GolangTest/Go_education/work/homework1.go
+++ GolangTest/Go_education/work/homework1.go
@@ -1,24 +1,16 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// SumInts возвращает сумму всех чисел nums.
-// Если срез пуст, должна вернуться 0.
-func SumInts(nums []int) int {
-	// TODO: реализуйте
-	var amount int
-	amount = 0
-	for i := 0; i < len(nums); i++ {
-		amount += nums[i]
-	}
-	return amount
-}
 func main() {
-	fmt.Println(SumInts([]int{1, 2, 3}))
-	fmt.Println(SumInts([]int{}))
-	// Ожидаемый вывод:
-	// 6
-	// 0
+	login := promtData("Введите логин")
+	password := promtData("Введите пароль")
+	url := promtData("Введите URL")
+}
+
+func promtData(promt string) {
+	fmt.Println(promt)
+	var res string
+	fmt.Scan(&res)
+	return res
 }
