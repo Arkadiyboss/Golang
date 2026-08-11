8e5fb9746d8fbc45454abac4c9e523d68380132c
Arkadiyboss <arkadiybosstrue@yandex.ru>
2026-08-11 15:05:11 +0300

    pet project

--- testSlice/validate_test.go
+++ testSlice/validate_test.go
@@ -1,2 +1,73 @@
-package validate_test
+package validate
 
+import (
+	"testing"
+)
+
+func TestEmptyPassword(t *testing.T) {
+	err := ValidatePassword("")
+
+	if err != ErrPasswordEmpty {
+		t.Fatalf("Ошибка не соответсвует ожидаемой")
+	}
+}
+
+func TestShortPassword(t *testing.T) {
+
+	err := ValidatePassword("1234567")
+
+	if err != ErrPasswordTooShort {
+		t.Fatalf("Ошибка не соответсвует ожидаемой")
+	}
+}
+
+func TestLongPassword(t *testing.T) {
+
+	err := ValidatePassword("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789123")
+
+	if err != ErrPasswordTooLong {
+		t.Fatalf("Ошибка не соответсвует ожидаемой")
+	}
+}
+
+func TestCommonWord(t *testing.T) {
+
+	err := ValidatePassword("12345678")
+
+	if err != ErrPasswordCommonWord {
+		t.Fatalf("Ошибка не соответсвует ожидаемой")
+	}
+}
+
+func TestPasswordHasSpace(t *testing.T) {
+	err := ValidatePassword("arkadiy ")
+
+	if err != ErrPasswordHasSpace {
+		t.Fatalf("Ошибка не соответсвует ожидаемой")
+	}
+}
+
+func TestUpperLetter(t *testing.T) {
+	err := ValidatePassword("arkadiy123")
+
+	if err != ErrPasswordNoUpper {
+		t.Fatalf("Ошибка не соответсвует ожидаемой")
+	}
+}
+
+
+func TestLowerLetter(t *testing.T) {
+	err := ValidatePassword("ARKADIY123")
+
+	if err != ErrPasswordNoLower {
+		t.Fatalf("Ошибка не соответсвует ожидаемой")
+	}
+}
+
+func TestNoDigit(t *testing.T) {
+	err := ValidatePassword("arkadiyYYYyyy")
+
+	if err != ErrPasswordNoDigit {
+		t.Fatalf("Ошибка не соответсвует ожидаемой")
+	}
+}
